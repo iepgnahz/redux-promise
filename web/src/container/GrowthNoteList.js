18115fb3f5049d0aeb4d69cea845c112c02d786f
zhangpei <pezhang@thoughtworks.com>
2017-09-08 14:23:25 +0800

    zhangpei - web - delete growth note app container

--- web/src/container/GrowthNoteList.js
+++
@@ -1,8 +0,0 @@
-import {connect} from 'react-redux';
-
-import  GrowthNoteList from '../component/GrowthNoteList';
-
-const mapStateToProps = state => ({growthNotes: state.growthNotes});
-const mapDispatchToProps = dispatch => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(GrowthNoteList);
