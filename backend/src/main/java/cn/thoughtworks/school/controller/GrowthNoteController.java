fcd794cd982369730ccb1f8ccba2e85e580b9032
intwzt <821854967@qq.com>
2017-09-08 15:29:33 +0800

    move service layer to controller layer

--- backend/src/main/java/cn/thoughtworks/school/controller/GrowthNoteController.java
+++ backend/src/main/java/cn/thoughtworks/school/controller/GrowthNoteController.java
@@ -2,12 +2,14 @@ package cn.thoughtworks.school.controller;
 
 
 import cn.thoughtworks.school.dao.GrowthNote;
-import cn.thoughtworks.school.service.GrowthNoteService;
+import cn.thoughtworks.school.repository.GrowthNoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,12 +18,15 @@ import java.util.Map;
 public class GrowthNoteController {
 
     @Autowired
-    private GrowthNoteService growthNoteService;
+    private GrowthNoteRepository growthNoteRepository;
+
+    @Autowired
+    EntityManager entityManager;
 
 
     @RequestMapping(value = "/{userId}/api/growthNotes", method = RequestMethod.GET)
     public ResponseEntity<List<GrowthNote>> getByAuthor(@RequestParam("author") int author) {
-        List<GrowthNote> growthNoteList = growthNoteService.getByAuthor(author);
+        List<GrowthNote> growthNoteList = growthNoteRepository.findByAuthor(author);
         if (0 == growthNoteList.size()){
             return new ResponseEntity<>(growthNoteList, HttpStatus.NOT_FOUND);
         }
@@ -30,7 +35,8 @@ public class GrowthNoteController {
 
     @RequestMapping(value = "/{userId}/api/growthNotes/{id}", method = RequestMethod.GET)
     public ResponseEntity<GrowthNote> getById(@PathVariable int id) {
-        GrowthNote growthNote = growthNoteService.getGrowthNoteRepository().findOne(id);
+
+        GrowthNote growthNote = growthNoteRepository.findOne(id);
         if (null == growthNote){
             return new ResponseEntity<>(growthNote, HttpStatus.NOT_FOUND);
         }
@@ -39,19 +45,31 @@ public class GrowthNoteController {
 
     @RequestMapping(value = "/{userId}/api/growthNotes/{rawId}", method = RequestMethod.DELETE)
     public ResponseEntity<Map<String, String>> delete(@PathVariable int userId, @PathVariable int rawId) {
-        Map<String, String> body = growthNoteService.delete(userId, rawId, GrowthNote.OperationType.DELETE);
+        GrowthNote lastGrowthNote = growthNoteRepository.findByRawIdAndAuthor(userId, rawId);
+        entityManager.detach(lastGrowthNote);
+        lastGrowthNote.setOperationType(GrowthNote.OperationType.DELETE);
+        lastGrowthNote.setId(null);
+        lastGrowthNote = growthNoteRepository.save(lastGrowthNote);
+        Map<String, String> body = new HashMap<>();
+        body.put("uri", "/users/" + userId + "/api/growthNotes/" + lastGrowthNote.getId());
         return new ResponseEntity<>(body, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/{userId}/api/growthNotes", method = RequestMethod.POST)
     public ResponseEntity<Map<String, String>> create(@PathVariable int userId,@RequestBody GrowthNote growthNote) {
-        Map<String, String> body = growthNoteService.create(growthNote, userId);
+        growthNote.setId(null);
+        growthNote = growthNoteRepository.save(growthNote);
+        Map<String, String> body = new HashMap<>();
+        body.put("uri", "/users/" + userId + "/api/growthNotes/" + growthNote.getId());
         return new ResponseEntity<>(body, HttpStatus.CREATED);
     }
 
-    @RequestMapping(value = "/{userId}/api/growthNotes/{rawId}", method = RequestMethod.PUT)
-    public ResponseEntity<Map<String, String>> update(@PathVariable int userId, @PathVariable int rawId, @RequestBody GrowthNote growthNote) {
-        Map<String, String> body = growthNoteService.update(growthNote, rawId, userId);
+    @RequestMapping(value = "/{userId}/api/growthNotes", method = RequestMethod.PUT)
+    public ResponseEntity<Map<String, String>> update(@PathVariable int userId, @RequestBody GrowthNote growthNote) {
+        growthNote.setId(null);
+        growthNote = growthNoteRepository.save(growthNote);
+        Map<String, String> body = new HashMap<>();
+        body.put("uri", "/users/" + userId + "/api/growthNotes/" + growthNote.getId());
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
 }
