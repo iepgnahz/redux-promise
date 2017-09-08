e18911eee63ae14a294d4245133e84ea93dc22c0
intwzt <821854967@qq.com>
2017-09-08 15:27:00 +0800

    add customer Query

--- backend/src/main/java/cn/thoughtworks/school/repository/GrowthNoteRepository.java
+++ backend/src/main/java/cn/thoughtworks/school/repository/GrowthNoteRepository.java
@@ -2,13 +2,19 @@ package cn.thoughtworks.school.repository;
 
 import cn.thoughtworks.school.dao.GrowthNote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface GrowthNoteRepository extends JpaRepository<GrowthNote, Integer> {
 
+    @Query(value = "select * from `growthNote` where id in " +
+            "(select max(id) from `growthNote` where author = ?1 group by rawId) " +
+            "and operationType in ('CREATE', 'UPDATE')", nativeQuery = true)
     List<GrowthNote> findByAuthor(int author);
 
-    List<GrowthNote> findByRawIdAndAuthor(int rarId, int author);
+    @Query(value = "select * from `growthNote` where id in " +
+            "(select max(id) as id from `growthNote` where author = ?1 and rawId = ?2)", nativeQuery = true)
+    GrowthNote findByRawIdAndAuthor(int author, int rawId);
 
 }
