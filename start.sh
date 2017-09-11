c88ddb9d8ea838f8228c469a6c749d11ebdaac77
zhangpei <pezhang@thoughtworks.com>
2017-09-11 18:46:58 +0800

    zhangpei - root - modify start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,11 @@
 
 docker-compose -f ./assembly/docker-compose.yml up -d
 cd backend
-./gradlew flywaymigrate
+./gradlew flywaymigratie
+cd ..
+cd ./web
+npm run build
+cd ..
+rm -rf ./backend/src/main/resources/static
+mkdir ./backend/src/main/resources/static
+cp -R ./web/build/ ./backend/src/main/resources/static
